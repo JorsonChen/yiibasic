f397f0c8aa065f57d5e87f7eb272d5f190f0e74c
JorsonChen <435178397@qq.com>
2016-01-31 13:40:17 +0800

    study gii

--- config/web.php
+++ config/web.php
@@ -45,10 +45,21 @@ $config = [
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
-
+    //$config['modules']['debug'] = 'yii\debug\Module';
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    //$config['modules']['gii'] = 'yii\gii\Module';
+
+    //配置多个ip
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '::1','10.1.192.258'],
+    ];
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1','10.1.192.258'],
+    ];
+
+
 }
 
 return $config;
